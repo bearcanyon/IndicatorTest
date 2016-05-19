47935ec1f831814bf6da2877537558ce7ab06ce8
KumagaiNaoki <j.a.r.n0405@gmail.com>
2016-05-19 18:08:05 +0900

    コミット

--- IndicatorSample/ViewController.swift
+++ IndicatorSample/ViewController.swift
@@ -6,20 +6,43 @@
 //  Copyright © 2016年 KumagaiNaoki. All rights reserved.
 //
 
+
 import UIKit
+import SVProgressHUD
 
 class ViewController: UIViewController {
-
+    
+    var indicator: UIActivityIndicatorView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func tapAppleButton(sender: UIButton) {
+        indicator = UIActivityIndicatorView()
+        indicator.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+        indicator.center = self.view.center
+        indicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.Gray
+        indicator.startAnimating()
+        self.view.addSubview(indicator)
     }
-
-
+    
+    @IBAction func tapSVProgressHUDButton(sender: UIButton) {
+        //        SVProgressHUD.show()
+        //        SVProgressHUD.showWithStatus("読み込み中")
+        //        SVProgressHUD.showSuccessWithStatus("成功！")//消える
+        SVProgressHUD.show()
+        //        SVProgressHUD.setDefaultAnimationType(SVProgressHUDAnimationType.Native)
+        //        SVProgressHUD.setDefaultMaskType(SVProgressHUDMaskType.Black)
+        SVProgressHUD.setDefaultStyle(.Dark)
+    }
+    
+    @IBAction func tapStopButton(sender: UIButton) {
+        indicator.stopAnimating()
+        
+        SVProgressHUD.dismiss()
+    }
 }
 
---
+++ Pods/Target Support Files/Pods-IndicatorSample/Pods-IndicatorSample-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_IndicatorSample : NSObject
+@end
+@implementation PodsDummy_Pods_IndicatorSample
+@end
